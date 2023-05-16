2d00480f4f4823c58e3845efa8d803dbdb4db6d1
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-05-17 01:39:12 +0300

    Fix 1-print_square.js and 3-user.py

--- 1-print_square.js
+++ 1-print_square.js
@@ -17,8 +17,9 @@ if (process.argv.length <= 2) {
 size = parseInt(process.argv[2], 16)
 
 for (let i = 0 ; i < size ; i ++) {
+    let row = '';
     for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+        row += process.stdout.write("#");
     }
     process.stdout.write("\n");
 }
