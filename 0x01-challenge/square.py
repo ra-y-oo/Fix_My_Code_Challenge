ed22bf114122bd7168541e86015f3569957396f8
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-12 20:39:53 +0300

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -26,7 +26,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(width=5, height=5)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
