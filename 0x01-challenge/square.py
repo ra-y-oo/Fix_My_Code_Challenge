23787e2fe8557efd15f59baecc6928f13f3597f8
ra-y-oo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-19 01:23:33 +0000

    update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
     width = 0
     height = 0
@@ -14,7 +14,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiterOfMySquare())
